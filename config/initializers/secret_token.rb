bb1debda7037156e3bf79f753679809a56b30178
Edward Anderson <nilbus@nilbus.com>
2013-01-04 19:29:08 -0500

    ruby rails/railties/bin/rails new pinewood-derby --dev # Rails 4 prerelease

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PinewoodDerby::Application.config.secret_key_base = 'be088b9fa90080069695ceff41f3ba896782530f450aeaf35f5505f800c53e4b0cc62582b6427cd841b035e9816ef20690311e724c0dcaf98105ee421e818b70'
