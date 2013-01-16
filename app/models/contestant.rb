b46a1a051e8254a48e32a99f3b25440e7afa61f2
Edward Anderson <nilbus@nilbus.com>
2013-01-16 00:42:40 -0500

    Correctly calculate heat/run count for generating the lineup

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -24,17 +24,18 @@ class Contestant < ActiveRecord::Base
     s = s.where('contestants.retired IS NOT TRUE')
     s = s.group('contestants.id')
     s = s.joins('LEFT JOIN runs ON runs.contestant_id = contestants.id')
-    s = s.having('count(runs.id) < ?', max_runs_per_contestant)
+    s = s.having('count(DISTINCT heats.id) < ?', max_runs_per_contestant)
     s = s.joins("LEFT JOIN runs AS runs_in_lane ON runs.contestant_id = contestants.id AND runs.lane = #{lane}")
     s = s.having("count(runs_in_lane.id) = 0")
     # order
     s = s.joins('LEFT JOIN heats ON heats.id = runs.heat_id')
     s = s.joins("LEFT JOIN runs AS runs_with_chosen ON runs_with_chosen.heat_id = heats.id AND runs_with_chosen.contestant_id IN (#{exclude.join(',')})")
     s = s.select('count(runs_with_chosen.id) AS heat_count_with_chosen')
-    s = s.select('count(runs.id) AS heat_count')
+    s = s.select('count(DISTINCT heats.id) AS heat_count')
     s = s.select('avg(runs.time) AS average_run_time')
     s = s.order('heat_count_with_chosen, heat_count, average_run_time DESC, contestants.created_at')
 
+    return s if options[:raw]
     s.first
   end
 
