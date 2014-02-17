7b669349ceec09a9b6e49394a55548b950e9d97b
Edward Anderson <nilbus@nilbus.com>
2014-02-16 20:47:46 -0500

    Alphabetize config file entries

--- app/models/derby_config.rb
+++ app/models/derby_config.rb
@@ -6,6 +6,12 @@ class DerbyConfig
 
   CONFIG_FILE = File.expand_path(File.join(__FILE__, '../../../config/derby_config.yml'))
 
+  def self.device_glob
+    instance['device_glob']
+  rescue KeyError
+    nil
+  end
+
   def self.lane_count
     instance['lane_count'].to_i
   end
@@ -14,12 +20,6 @@ class DerbyConfig
     sensor_class_name = instance['sensor_class_name']
 
     TrackSensor.const_get(sensor_class_name)
-  end
-
-  def self.device_glob
-    instance['device_glob']
-  rescue KeyError
-    nil
   end
 
   def initialize
