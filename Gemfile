dec380b492e541cb26bf4f2260547696d8ca297f
Edward Anderson <nilbus@nilbus.com>
2014-12-20 12:55:16 -0800

    Switch database backend from PostgreSQL to SQLite

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '~> 4.1.0.beta1'
 gem 'thin-rails'
-gem 'pg'
+gem 'sqlite3'
 gem 'daemons-rails'
 gem 'faye-rails', github: 'nilbus/faye-rails', branch: 'rails4'
 gem 'faye-redis'
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130109190837) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "contestants", force: true do |t|
     t.string   "name"
     t.boolean  "retired"
