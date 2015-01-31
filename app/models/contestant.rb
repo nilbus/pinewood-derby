nilbus/pinewood-derby

commit dd162f2151277634889c6ad1b874414d5d63e6d7

Order contestant roster by rank or name

app/models/contestant.rb
@@ -3,11 +3,15 @@ class Contestant < ActiveRecord::Base
   has_many :heats, through: :runs
 
   scope :ranked, -> do
-    select('contestants.*, avg(runs.time) AS average_time').
-    joins(:runs => :heat).
+    with_average_time.
     where('heats.sequence >= 0').
-    group('contestants.id').
     order('average_time')
+  end
+
+  scope :with_average_time, -> do
+    select('contestants.*, avg(runs.time) AS average_time').
+    group('contestants.id').
+    joins(:runs => :heat)
   end
 
   validates :name, presence: true

app/controllers/contestants_controller.rb
@@ -5,7 +5,13 @@ class ContestantsController < ApplicationController
   # GET /contestants
   # GET /contestants.json
   def index
-    @contestants = Contestant.order('id')
+    @contestants = Contestant.with_average_time.to_a.sort_by do |contestant|
+      if contestant.average_time
+        [contestant.average_time, contestant.name]
+      else
+        [99, contestant.name]
+      end
+    end
 
     respond_to do |format|
       format.html # index.html.erb

commit 28155274c1e5c6334dd52b3ea2fc89aa001b898c

Fix missing contestant issue on contestants page
Only contestants who had been lined up showed up because of the inner join.
Bug introduced in dd162f2.

app/models/contestant.rb
@@ -11,7 +11,7 @@ class Contestant < ActiveRecord::Base
   scope :with_average_time, -> do
     select('contestants.*, avg(runs.time) AS average_time').
     group('contestants.id').
-    joins(:runs => :heat)
+    joins('LEFT JOIN runs ON runs.contestant_id = contestants.id LEFT JOIN heats ON heats.id = runs.heat_id')
   end
 
   validates :name, presence: true